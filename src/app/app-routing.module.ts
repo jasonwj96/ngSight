eb06b7dc3f9417174406ba7cc453e7ead1798a37
jasonwj96 <jasonwj96@gmail.com>
2019-05-30 16:55:15 -0500

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SectionHealthComponent } from './sections/section-health/section-health.component';
+import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
+import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'sales',
+    component: SectionSalesComponent
+  },
+  {
+    path: 'health',
+    component: SectionHealthComponent
+  },
+  {
+    path: 'orders',
+    component: SectionOrdersComponent
+  },
+  { path: '', redirectTo: 'sales', pathMatch: 'full' },
+  { path: '**', redirectTo: 'sales' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
