8838886ddb9aa89c5af912494b7c6d765081bcbf
jasonwj96 <jasonwj96@gmail.com>
2019-06-03 20:01:57 -0500

    Add orders table. Order, customer models

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -6,10 +7,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section-orders.component.scss']
 })
 export class SectionOrdersComponent implements OnInit {
+  orders: Order[] = [
+    {
+      id: 1,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com'
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: 'Completed'
+    },
+    {
+      id: 2,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com'
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: 'Completed'
+    },
+    {
+      id: 3,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com'
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: 'Completed'
+    },
+    {
+      id: 4,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com'
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: 'Completed'
+    },
+    {
+      id: 5,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com'
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: 'Completed'
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
